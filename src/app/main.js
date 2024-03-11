a5f3049ca831d140a3fc00d361be954a27c76885
ahiakwogreat77@gmail.com <ahiakwogreat77@gmail.com>
2024-03-11 08:33:05 -0700

    added naavigation

---
+++ src/app/main.js
@@ -0,0 +1,45 @@
+const toggleBtn=document.getElementById('menu-toggle');
+
+
+function navToggle() {
+  const nav= document.querySelector(".primary-nagivation");
+  if () {
+    
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+toggleBtn.addEventListener('click', navToggle)
